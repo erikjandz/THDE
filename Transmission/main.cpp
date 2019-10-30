cb737e9725c3a783e4c6aa0e4c398673a1ef8146
Youri de Vor <youri.devor@student.hu.nl>
2019-10-30 11:17:09 +0100

    Update main.cpp
    Oude code eruit

--- Transmission/main.cpp
+++ Transmission/main.cpp
@@ -3,37 +3,8 @@
 #include "receiver.hpp"
 #include <array>
 
-/*void transmit(){
-  auto ir = hwlib::target::d2_36kHz();
-  auto transmitter = Send_IR_Message_Control(ir, 15, 2);
-  //transmitter.sendMessage();
-}
-
-void receive(){
-  namespace target = hwlib::target;
-
-  auto tsop_signal = target::pin_in( target::pins::d8 );
-  auto tsop_gnd    = target::pin_out( target::pins::d9 );
-  auto tsop_vdd    = target::pin_out( target::pins::d10 );
-  tsop_gnd.write( 0 );
-  tsop_vdd.write( 1 );
-  tsop_gnd.flush();
-  tsop_vdd.flush();
-
-  auto receiver = Receive_IR_Message_Control( tsop_signal );
-  for(;;){
-      std::array<bool, 16> array = receiver.receiveMessage();
-      for(auto i  : array){
-          hwlib::cout << i;
-      }
-      hwlib::cout << hwlib::endl;
-      receiver.decode(array);
-  }
-}*/
 
 int main(){
-    //transmit();
-    //receive();
     WDT->WDT_MR = WDT_MR_WDDIS;
     namespace target = hwlib::target;
     hwlib::wait_ms( 1000 );
