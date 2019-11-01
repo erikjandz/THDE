115d066f8a2afb59884c8f0383a18bcc8684ad2c
erikjandz <erik.dezeeuw@student.hu.nl>
2019-11-01 07:08:06 -0700

    updates

--- SRC/Game_Parameter_Control.hpp
+++ SRC/Game_Parameter_Control.hpp
@@ -1,16 +1,36 @@
 #pragma once
 #include "hwlib.hpp"
 #include "rtos.hpp"
+#include "Receive_IR_Listener.hpp"
 
 class Game_Parameter_Control: public Receive_IR_Listener, public rtos::task<>{
 public:
     Game_Parameter_Control(){
       _playerIDPool.write(5);
+      _weaponPowerPool.write(9);
     }
 
     void main()override{
         enum class state_t{IDLE, DECODE, ACTIVE};
-
+        state_t state = IDLE;
+        for(;;){
+            switch( state ){
+                case IDLE:
+                    wait( _MessageFlag );
+                    state = DECODE;
+                    break;
+                case DECODE:
+                    if(_GetPlayerID == 0){
+                        state = ACTIVE;
+                    }else{
+                        state = IDLE;
+                    }
+                    break;
+                case ACTIVE:
+                    state = IDLE;
+                    break;
+            }
+        }
     }
 
     int getPlayerID(){
