7e8fbc64814f17623cd56df39a1f58b4ea8c158c
ConsolePeasant92 <hugocornel@yahoo.com>
2019-11-01 09:22:56 +0100

    updates

---
+++ SRC/IR_sender.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include "hwlib.hpp"
+#include "rtos.hpp"
+
+class IR_sender{
+public:
+    IR_sender(hwlib::target::d2_36kHz & _pin):
+        _pin( _pin )
+        {}
+    void ledOn(){
+        _pin.write( 1 );
+    }
+
+    void ledOff(){
+        _pin.write( 0 );
+    }
+private:
+    hwlib::target::d2_36kHz & _pin;
+};
