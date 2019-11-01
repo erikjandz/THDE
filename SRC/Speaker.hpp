8ccbe51da5162678650d1ae3a4d8f0d4912b5d65
Youri <youridv1@gmail.com>
2019-11-01 13:37:30 +0100

    updates

--- SRC/Speaker.hpp
+++ SRC/Speaker.hpp
@@ -2,21 +2,44 @@
 #include "hwlib.hpp"
 #include "beep.hpp"
 
-class Speaker {
+class Speaker : public rtos::task<> {
 public:
   Speaker(hwlib::pin_out& pin): _pin( pin ) {}
-  void playEndTone(){
-    beep::ohhh( _pin );
-  }
+  void playEndTone(){ _end_tone_flag.set(); }
+  void playShootTone(){ _shoot_tone_flag.set(); }
+  void playDeathTone(){ _death_tone_flag.set(); }
 
-  void playShootTone(){
-    beep::peew( _pin );
-  }
-
-  void playDeathTone(){
-    beep::uhoh( _pin );
+  void main() override {
+    enum class state_t{IDLE, ENDTONE, SHOOTTONE, DEATHTONE};
+    state_t state = IDLE;
+    for(;;){
+        switch(state){
+            case IDLE:
+                auto event = wait(_end_tone_flag + _shoot_tone_flag + _death_tone_flag);
+                if( event == _end_tone_flag   ){ state = ENDTONE;   }
+                if( event == _shoot_tone_flag ){ state = SHOOTTONE; }
+                if( event == _death_tone_flag ){ state = DEATHTONE; }
+                break;
+            case ENDTONE:
+                beep::ohhh( _pin );
+                state = IDLE;
+                break;
+            case SHOOTTONE:
+                beep::peew( _pin );
+                state = IDLE;
+                break;
+            case DEATHTONE:
+                beep::uhoh( _pin );
+                state = IDLE;
+                break;
+        }
+    }
   }
 
 private:
   hwlib::target::pin_out& _pin;
+  rtos::flag _end_tone_flag;
+  rtos::flag _shoot_tone_flag;
+  rtos::flag _death_tone_flag;
+
 };
