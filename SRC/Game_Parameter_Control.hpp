14e2b89cea28bb1a8412ac517339990b46604e42
erikjandz <erik.dezeeuw@student.hu.nl>
2019-11-05 02:28:19 -0800

    updates

---
+++ SRC/Game_Parameter_Control.hpp
@@ -0,0 +1,96 @@
+#pragma once
+#include "hwlib.hpp"
+#include "rtos.hpp"
+#include "Receive_IR_Listener.hpp"
+#include "Time_Run_Control.hpp"
+
+class Game_Parameter_Control: public Receive_IR_Listener{
+public:
+    Game_Parameter_Control(keyPad & keypad, Oled_Display & display, Time_Run_Control & time_run_control):
+        _keypad( keypad ),
+        _display( display ),
+        _time_run_control( time_run_control)
+    {
+      _playerIDPool.write(1);
+      _weaponPowerPool.write(0);
+    }
+
+    void main()override{
+        char key;
+        for(;;){
+            switch( _state ){
+                case State::IDLE:
+                    hwlib::wait_ms(60);
+                    if(_keypad.getKeyPressed() == 'A'){
+                        _state = State::A_PRESSED;
+                    }
+                    break;
+                case State::A_PRESSED:
+                    hwlib::wait_ms(60);
+                    key = _keypad.getKeyPressed();
+                    if( key >= '0' && key <= '9' ){
+                        _playerID = key - 48;
+                        _state = State::FIRST_NUMBER_ENTERED;
+                    }
+                    break;
+                case State::FIRST_NUMBER_ENTERED:
+                    hwlib::wait_ms(60);
+                    if(_keypad.getKeyPressed() == 'B'){
+                        _state = State::B_PRESSED;
+                    }
+                    break;
+                case State::B_PRESSED:
+                    hwlib::wait_ms(60);
+                    key = _keypad.getKeyPressed();
+                    if( key >= '0' && key <= '9' ){
+                        _weaponPower = key - 48;
+                        _state = State::SECOND_NUMBER_ENTERED;
+                    }
+                    break;
+                case State::SECOND_NUMBER_ENTERED:
+                    _playerIDPool.write(_playerID);
+                    _weaponPowerPool.write(_weaponPower);
+                    _state = State::WAITING_FOR_LEADER;
+                    break;
+                case State::WAITING_FOR_LEADER:
+                    wait( _messageFlag );
+                    _state = State::IF_LEADER;
+                    break;
+                case State::IF_LEADER:
+                    //if you get a message from the leader
+                    if(_GetPlayerID(_MessagePool.read()) == 0){
+                        _state = State::ACTIVE;
+                    }else{
+                        _state = State::WAITING_FOR_LEADER;
+                    }
+                    break;
+                case State::ACTIVE:
+                    //decode the time and give it to time_run_control
+                    _time_run_control.setTime(_GetWeaponPower(_messagePool.read()));
+                    hwlib::wait_us(60);
+                    _state = State::IDLE;
+                    break;
+            }
+        }
+    }
+
+
+    int getPlayerID(){
+        return _playerIDPool.read();
+    }
+
+    int getWeaponPower(){
+        return _weaponPowerPool.read();
+    }
+private:
+    int _playerID;
+    int _weaponPower;
+    rtos::pool< int > _playerIDPool;
+    rtos::pool< int > _weaponPowerPool;
+    keyPad & _keypad;
+    Oled_Display & _display;
+    Time_Run_Control & _time_run_control;
+
+    enum class State { IDLE, WAITING_FOR_LEADER, ACTIVE, IF_LEADER, A_PRESSED, B_PRESSED, FIRST_NUMBER_ENTERED, SECOND_NUMBER_ENTERED};
+    State _state = State::IDLE;
+};
--- SRC/Init_Game_Control.hpp
+++ SRC/Init_Game_Control.hpp
@@ -13,23 +13,39 @@ public:
 
   void main()override{
     char k;
+    char key;
     for(;;){
       switch( _state ){
         case State::IDLE:
           hwlib::wait_ms( 60 );
           k = _keyPad.getKeyPressed();
-          if(k == 'c'){
+          if(k == 'C'){
             _state = State::KEYPRESSED;
           }
           break;
         case State::KEYPRESSED:
+          hwlib;:wait_ms( 60 );
           _display.showText("enter key");
           k = _keyPad.getKeyPressed();
           if(k >= '0' && k <= '9'){
+            key = k;
             _display.clear();
-            _display.showText(&k);
+            _display.showText(k);
             _display.showText(" pressed");
+            _state = State::NUMBER_ENTERED;
           }
+          break;
+        case State::NUMBER_ENTERED:
+          hwlib::wait_ms( 60 );
+          k = _keyPad.getKeyPressed();
+          if(k == '#'){
+            _state = State::DONE;
+          }
+          break;
+        case State::DONE:
+          _send_ir_message_control.send_message(0, key - 48);
+          hwlib::wait_ms(1000000);
+          break;
       }
     }
 
@@ -40,6 +56,34 @@ private:
   Oled_Display & _display;
   Send_IR_Message_Control & _send_ir_message_control;
 
-  enum class State { IDLE, KEYPRESSED, };
+  enum class State { IDLE, KEYPRESSED, NUMBER_ENTERED, DONE};
   State _state = State::IDLE;
+
+  void decode(int playerID, int weaponPower){
+      //decode the message to a bool array ready to send
+      _message[0] = 1;
+      int index = 1;
+      for(int i = 16; i >= 1; i /= 2){
+        if(_playerID >= i){
+          _playerID -= i;
+          _message[index] = 1;
+        }else{
+          _message[index] = 0;
+        }
+        index ++;
+      }
+      for(int i = 16; i >= 1; i /= 2){
+        if(_weaponPower >= i){
+          _weaponPower -= i;
+          _message[index] = 1;
+        }else{
+          _message[index] = 0;
+        }
+        index ++;
+      }
+      //add the control bits
+      for(int i = 1; i < 6; i ++){
+        _message[10 + i] = _message[i] ^ _message[i + 5];
+      }
+  }
 };
